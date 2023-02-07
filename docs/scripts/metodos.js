4b8a0037f2ab98991a30f3b1354a13ee6cb25612
Cristhian <cristhianandres2001@gmail.com>
2023-02-07 12:45:31 -0500

    solucione error de axios

--- docs/scripts/metodos.js
+++ docs/scripts/metodos.js
@@ -12,11 +12,11 @@
       });
   });
   */
-
+/*
   document.getElementById("api").addEventListener("click", function(event) {
     event.preventDefault();
     let searchTerm = document.getElementById("searchTerm").value;
-    axios.get(`http://localhost:8080/api/browser/${searchTerm}`, {mode: 'no-cors'})
+    axios.get(`http://localhost:8080/api/browser/${searchTerm}`)
       .then(response => {
         let responseContainer = document.getElementById("response");
         responseContainer.innerText = JSON.stringify(response.data);
@@ -24,4 +24,35 @@
       .catch(error => {
         console.error('Ha ocurrido un error:', error);
       });
+  });
+
+*/
+  document.getElementById("api").addEventListener("click", function(event) {
+    event.preventDefault();
+    let searchTerm = document.getElementById("searchTerm").value;
+    axios.get(`http://localhost:8080/api/browser/${searchTerm}`)
+    .then(response => {
+      let responseContainer = document.getElementById("response");
+      let peliculas = response.data;
+      let resultados = '';
+      peliculas.forEach(pelicula => {
+        resultados += `
+          <p>Código: ${pelicula.codigo}</p>
+          <p>Nombre: ${pelicula.nombre}</p>
+          <p>Sinopsis: ${pelicula.sipnosis}</p>
+          <iframe width="560" height="315" src="${pelicula.url_Trailer}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <!--<p>URL del trailer: ${pelicula.url_Trailer}</p>-->
+          <img src="${pelicula.url_Imagen}" alt="Image">
+          <!--<p>URL de la imagen: ${pelicula.url_Imagen}</p>-->
+          <p>Estado: ${pelicula.estado}</p>
+          <p>Reparto: ${pelicula.reparto}</p>
+          <br>
+        `;
+      });
+      responseContainer.innerHTML = resultados;
+    })
+    .catch(error => {
+      console.error('Ha ocurrido un error:', error);
+    });
+
   });
--- negocio/src/main/java/co/edu/uniquindio/cineprime/controllers/UsuarioApi.java
+++ negocio/src/main/java/co/edu/uniquindio/cineprime/controllers/UsuarioApi.java
@@ -4,15 +4,13 @@ package co.edu.uniquindio.cineprime.controllers;
 import co.edu.uniquindio.cineprime.entidades.Pelicula;
 import co.edu.uniquindio.cineprime.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class UsuarioApi {
 
     @Autowired
